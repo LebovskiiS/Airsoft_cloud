db60506823cb24e4bae22e77c90cf397b5e19c7e
lebovskii <lebovskiil@gmail.com>
2024-08-31 23:52:42 -0700

    db_1.0.1

--- db_scripts.py
+++ db_scripts.py
@@ -1,1 +1,11 @@
-create_table = 'CREATE TABLE information (ID PRIMARY KEY AUTOINCREMENT name TEXT, nickname TEXT, event_name TEXT, day INTEGER, location TEXT, link TEXT NOT NULL)'
+create_table = '''
+CREATE TABLE IF NOT EXISTS information (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    nickname TEXT,
+    event_name TEXT,
+    day INTEGER,
+    location TEXT,
+    link TEXT NOT NULL
+)
+'''
